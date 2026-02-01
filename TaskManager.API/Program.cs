acbf05a0790abfd3ced603ee687a8c0ff346bfc1
Bruno Calera <brunocalerasp@gmail.com>
2026-02-01 12:04:25 -0300

    feat: Criando entidade Task
    feat: Criado Enums de Priority e Status

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddRouting(opt => opt.LowercaseUrls = true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +20,11 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "petfolio-api v1");
+        options.RoutePrefix = string.Empty; // Swagger direto na raiz (localhost:5000/)
+    });
 }
 
 app.UseHttpsRedirection();
---
+++ TaskManager.Domain/Entities/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.VisualBasic;
+using TaskManager.Domain.Enums;
+
+namespace TaskManager.Domain.Entities
+{
+    public class Task
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "O campo Name é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo Name não pode ter mais de 100 caracteres")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "O campo Description não pode ter mais de 500 caracteres")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo Priority é obrigatório")]
+        public Priority Priority { get; set; }
+
+        [Required(ErrorMessage = "O campo DueDate é obrigatório")]
+        public DateTime DueDate { get; set; }
+
+        [Required(ErrorMessage = "O campo Status é obrigatório")]
+        public Status Status { get; set; }
+    }
+}
